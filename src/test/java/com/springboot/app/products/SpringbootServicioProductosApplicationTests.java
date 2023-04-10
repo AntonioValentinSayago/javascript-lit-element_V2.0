1ac5c9dc40ab25ab62a00360531ab2518287ddfc
AntonioValentin <cesar_anto30@hotmail.com>
2023-04-10 15:19:53 -0600

    Creation of the Structure

---
+++ src/test/java/com/springboot/app/products/SpringbootServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
